da337d1fddbd4d546d692cde2ae07408c87c6209
YSNascimento <yuri.nascimento.ro@gmail.com>
2020-06-25 17:24:28 -0300

    Modal em receitas.html ok

---
+++ layouts/front/receitas.js
@@ -0,0 +1,26 @@
+const modalOverley = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+// const imgs = document.querySelectorAll('.card .card-img')
+// const titles = document.querySelectorAll('.card .card-title')
+// const authors = document.querySelectorAll('.card .card-author')
+
+for (let card of cards){
+    card.addEventListener("click", function(){
+        const imgSrc = card.childNodes[1].src
+        const title = card.childNodes[3].innerHTML
+        const author = card.childNodes[5].innerHTML
+        console.log(imgSrc)
+        console.log(title)
+        console.log(author)
+        modalOverley.classList.add('active')
+        modalOverley.querySelector('.modal .modal-img').src= imgSrc
+        modalOverley.querySelector('.modal .modal-title').innerHTML= title
+        modalOverley.querySelector('.modal .modal-author').innerHTML= author
+    })
+}
+
+document.querySelector('.modal-close')
+    .addEventListener("click", function() {
+        modalOverley.classList.remove('active')
+        // modalOverley.querySelector('iframe').src= ""
+})
